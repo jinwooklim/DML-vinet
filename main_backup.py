517a1feb8e4d786a6fcfd24143dd76d3c040d72c
jinwooklim <aidenlim94@gmail.com>
2018-06-28 19:52:09 +0900

    rev main.py

--- main_backup.py
+++ main_backup.py
@@ -26,82 +26,66 @@ from PIL import Image
 import csv
 import time
 
-def toQuaternion(yaw, pitch, roll):
-    
-    cy = np.cos(yaw * 0.5)
-    sy = np.sin(yaw * 0.5)
-    cr = np.cos(roll * 0.5)
-    sr = np.sin(roll * 0.5)
-    cp = np.cos(pitch * 0.5)
-    sp = np.sin(pitch * 0.5)
-
-    qw = cy * cr * cp + sy * sr * sp
-    qx = cy * sr * cp - sy * cr * sp
-    qy = cy * cr * sp + sy * sr * cp
-    qz = sy * cr * cp - cy * sr * sp
-
-    #print(qw, qx, qy, qz)    
-
-    return [qw,qx,qy,qz]
 
 
 class MyDataset:
     
-    def __init__(self, base_dir, sequence): # (self, Total_Data, 00)
+    def __init__(self, base_dir, sequence):
         self.base_dir = base_dir
         self.sequence = sequence
-        #self.base_path_img = self.base_dir + self.sequence + '/cam0/data/'
-        self.base_path_img = os.path.join(self.base_dir, 'KITTI_odometry', 'dataset', 'sequences', sequence, 'image_2')
+        self.base_path_img = self.base_dir + self.sequence + '/cam0/data/'
         
-        #self.data_files = os.listdir(self.base_dir + self.sequence + '/cam0/data/')
-        self.data_files = os.listdir(os.path.join(self.base_dir, 'KITTI_odometry', 'dataset', 'sequences', sequence, 'image_2/'))
+        
+        self.data_files = os.listdir(self.base_dir + self.sequence + '/cam0/data/')
         self.data_files.sort()
         
         ## relative camera pose
-        #self.trajectory_relative = self.read_R6TrajFile('/vicon0/sampled_relative_R6.csv')
-        self.trajectory_relative = self.read_R6TrajFile(os.path.join("relative_pose", sequence+".txt"))        
+        self.trajectory_relative = self.read_R6TrajFile('/vicon0/sampled_relative_R6.csv')
         
-        ## abosolute camera pose (global)
-        #self.trajectory_abs = self.readTrajectoryFile('/vicon0/sampled.csv')
-        self.trajectory_abs = self.readTrajectoryFile(os.path.join("pose", sequence+".txt"))
+		## abosolute camera pose (global)
+        self.trajectory_abs = self.readTrajectoryFile('/vicon0/sampled.csv')
         
-        ## imu
-        #self.imu = self.readIMU_File('/imu0/data.csv')
-        self.imu = self.readIMU_File(os.path.join("virtual_obd", sequence, "0.txt"))
+		## imu
+        self.imu = self.readIMU_File('/imu0/data.csv')
         
-        self.imu_seq_len = 3
+        self.imu_seq_len = 5
    
-    def read_R6TrajFile(self, path):
-        traj = []
-        with open(os.path.join(self.base_dir, path)) as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            for row in spamreader:
-                #parsed = [float(row[0]), float(row[1]), float(row[2]), -float(row[3]), float(row[4]), float(row[5])]
-                parsed = [float(row[2]), -float(row[0]), float(row[1]), -float(row[3]), float(row[4]), float(row[5])]
-                traj.append(parsed)
-        return np.array(traj)
-
     def readTrajectoryFile(self, path):
         traj = []
-        with open(os.path.join(self.base_dir, path)) as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        with open(self.base_dir + self.sequence + path) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in spamreader:
-                # x, y, z, yaw, pitch, roll -> x, y, z, ww, wx, wy, wz
-                quaternion = toQuaternion(-float(row[3]), float(row[4]), float(row[5]))
-                parsed = [float(row[2]), -float(row[0]), float(row[1]), 
-                quaternion[0], quaternion[1], quaternion[2], quaternion[3]]
+                parsed = [float(row[1]), float(row[2]), float(row[3]), 
+                          float(row[4]), float(row[5]), float(row[6]), float(row[7])]
                 traj.append(parsed)
+                
         return np.array(traj)
-
-    def readIMU_File(self, path):
-        obd = []
-        count = 0
-        with open(os.path.join(self.base_dir, path)) as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    
+    def read_R6TrajFile(self, path):
+        traj = []
+        with open(self.base_dir + self.sequence + path) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in spamreader:
-                parsed = [float(row[0]), float(row[1])]
-                obd.append(parsed)
-        return np.array(obd)
+                parsed = [float(row[1]), float(row[2]), float(row[3]), 
+                          float(row[4]), float(row[5]), float(row[6])]
+                traj.append(parsed)
+                
+        return np.array(traj)
+    
+    def readIMU_File(self, path):
+        imu = []
+        count = 0
+        with open(self.base_dir + self.sequence + path) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in spamreader:
+                if count == 0:
+                    count += 1
+                    continue
+                parsed = [float(row[1]), float(row[2]), float(row[3]), 
+                          float(row[4]), float(row[5]), float(row[6])]
+                imu.append(parsed)
+                
+        return np.array(imu)
 
     def getTrajectoryAbs(self, idx):
         return self.trajectory_abs[idx]
@@ -116,25 +100,21 @@ class MyDataset:
         return len(self.trajectory_relative)
     
     def load_img_bat(self, idx, batch):
+        # batch -> Sequence length
         batch_x = []
         batch_imu = []
         for i in range(batch):
-            #image01 = (Image.open(os.path.join(self.base_path_img, self.data_files[idx + i]))).crop((200, 0, 1040, 375))
-            #image02 = (Image.open(os.path.join(self.base_path_img, self.data_files[idx+1 + i]))).crop((200, 0, 1040, 375))
-            image01 = Image.open(os.path.join(self.base_path_img, self.data_files[idx + i]))
-            image02 = Image.open(os.path.join(self.base_path_img, self.data_files[idx+1 + i]))
-            image01 = image01.resize((620, 376), resample=Image.BICUBIC)
-            image02 = image02.resize((620, 376), resample=Image.BICUBIC)
-            
-            x_data_np_1 = np.array(image01)
-            x_data_np_2 = np.array(image02)
-            
-            # 3 channel
-            #x_data_np_1 = np.array([x_data_np_1, x_data_np_1, x_data_np_1])
-            #x_data_np_2 = np.array([x_data_np_2, x_data_np_2, x_data_np_2])
+            x_data_np_1 = np.array(Image.open(self.base_path_img + self.data_files[idx + i]))
+            x_data_np_2 = np.array(Image.open(self.base_path_img + self.data_files[idx+1 + i]))
+
+            ## 3 channels
+            x_data_np_1 = np.array([x_data_np_1, x_data_np_1, x_data_np_1])
+            x_data_np_2 = np.array([x_data_np_2, x_data_np_2, x_data_np_2])
+
             X = np.array([x_data_np_1, x_data_np_2])
             batch_x.append(X)
-
+            
+            
             tmp = np.array(self.imu[idx-self.imu_seq_len+1 + i:idx+1 + i])
             batch_imu.append(tmp)
             
@@ -150,7 +130,7 @@ class MyDataset:
         
         ## global pose gt
         Y2 = Variable(torch.from_numpy(self.trajectory_abs[idx+1:idx+1+batch]).type(torch.FloatTensor).cuda())
-      
+        
         return X, X2, Y, Y2
 
     
@@ -159,15 +139,15 @@ class Vinet(nn.Module):
     def __init__(self):
         super(Vinet, self).__init__()
         self.rnn = nn.LSTM(
-            input_size=49161, #49152,#24576, 
-            hidden_size=1024, #64, 
+            input_size=49165,#49152,#24576, 
+            hidden_size=1024,#64, 
             num_layers=2,
             batch_first=True)
         self.rnn.cuda()
         
         self.rnnIMU = nn.LSTM(
-            input_size=2, #6, 
-            hidden_size=2, #6,
+            input_size=6, 
+            hidden_size=6,
             num_layers=2,
             batch_first=True)
         self.rnnIMU.cuda()
@@ -197,8 +177,7 @@ class Vinet(nn.Module):
         self.flownet_c.cuda()
 
     def forward(self, image, imu, xyzQ):
-        batch_size, timesteps, H, W, C = image.size()
-
+        batch_size, timesteps, C, H, W = image.size()
         ## Input1: Feed image pairs to FlownetC
         c_in = image.view(batch_size, timesteps * C, H, W)
         c_out = self.flownet_c(c_in)
@@ -217,10 +196,13 @@ class Vinet(nn.Module):
         r_in = c_out.view(batch_size, 1, -1)
         #print('r_in', r_in.shape)
         
-
+        #cat_out = torch.cat((r_in, imu_out), 2)#1 1 49158
+        #cat_out = torch.cat((cat_out, xyzQ), 2)#1 1 49165
         cat_out = torch.cat((r_in, imu_out), 2)#1 1 49158
+        #print("cat_out : ", np.shape(cat_out))
+        #print("xyzQ : ", np.shape(xyzQ))
         cat_out = torch.cat((cat_out, xyzQ), 2)#1 1 49165
-                
+        #print(np.shape(cat_out))
         r_out, (h_n, h_c) = self.rnn(cat_out)
         l_out1 = self.linear1(r_out[:,-1,:])
         l_out2 = self.linear2(l_out1)
@@ -229,33 +211,30 @@ class Vinet(nn.Module):
         return l_out2
     
     
-def model_out_to_flow_png(output, name):
-    out_np = output
-    print(np.shape(out_np))
+def model_out_to_flow_png(output):
+    out_np = output[0].data.cpu().numpy()
+
     #https://gitorchub.com/DediGadot/PatchBatch/blob/master/flowlib.py
     out_np = np.squeeze(out_np)
-    print(np.shape(out_np))
-    #out_np = np.moveaxis(out_np, 0, -1)
+    out_np = np.moveaxis(out_np,0, -1)
 
     im_arr = flowlib.flow_to_image(out_np)
     im = Image.fromarray(im_arr)
-    im.save("%s.png"%name)
+    im.save('test.png')
 
 
 def train():
     epoch = 20
-    batch = 1
+    batch = 4
     model = Vinet()
-    #optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     #optimizer = optim.Adam(model.parameters(), lr = 0.001)
     
     writer = SummaryWriter()
     
     model.train()
 
-	#mydataset = MyDataset('/notebooks/EuRoC_modify/', 'V1_01_easy')
-    mydataset = MyDataset('/notebooks/data/Total_Data/', '00')
+    mydataset = MyDataset('/notebooks/EuRoC_modify/', 'V1_01_easy')
     #criterion  = nn.MSELoss()
     criterion  = nn.L1Loss(size_average=False)
     
@@ -271,31 +250,51 @@ def train():
                 data, data_imu, target_f2f, target_global = mydataset.load_img_bat(i, batch)
                 data, data_imu, target_f2f, target_global = \
                     data.cuda(), data_imu.cuda(), target_f2f.cuda(), target_global.cuda()
-
+                
                 optimizer.zero_grad()
                 
+                '''            
                 if i == start:
                     ## load first SE3 pose xyzQuaternion
                     abs_traj = mydataset.getTrajectoryAbs(start)
+                    
                     abs_traj_input = np.expand_dims(abs_traj, axis=0)
                     abs_traj_input = np.expand_dims(abs_traj_input, axis=0)
                     abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda()) 
-                
+                '''
+                   
+                if i == start:
+                    ## load first SE3 pose xyzQuaternion
+                    abs_traj = mydataset.getTrajectoryAbs(start)
+                    abs_traj_input = np.expand_dims(abs_traj, axis=0) # (1,7)
+                    abs_traj_input = np.expand_dims(abs_traj_input, axis=0) # (1,1,7)
+                    abs_traj_input = np.tile(abs_traj_input, (batch,1,1)) # (4,1,7)
+                    abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda()) 
+                    
+                    abs_traj_batch = np.tile(abs_traj, (batch,1)) ########### Using for SE part
+
                 ## Forward
                 output = model(data, data_imu, abs_traj_input)
                 
                 ## Accumulate pose
                 numarr = output.data.cpu().numpy()
-                
-                abs_traj = se3qua.accu(abs_traj, numarr) # abs_traj : 7, numarr : 6
-                
+              
+                ## SE part 
+                abs_traj_list = []
+                for idx in range(batch):
+                    abs_traj_temp = se3qua.accu(abs_traj_batch[idx,...], numarr[idx,...])
+                    abs_traj_list.append(abs_traj_temp)
+                abs_traj_batch = np.asarray(abs_traj_batch)
+                 
                 abs_traj_input = np.expand_dims(abs_traj, axis=0)
                 abs_traj_input = np.expand_dims(abs_traj_input, axis=0)
+                abs_traj_input = np.tile(abs_traj_input, (batch,1,1))
                 abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda()) 
-                
+
                 ## (F2F loss) + (Global pose loss)
                 ## Global pose: Full concatenated pose relative to the start of the sequence
                 loss = criterion(output, target_f2f) + criterion(abs_traj_input, target_global)
+
                 loss.backward()
                 optimizer.step()
 
@@ -326,7 +325,8 @@ def train():
     writer.close()
 
 def test():
-    checkpoint_pytorch = '/notebooks/vinet/vinet_v1_01.pt'
+    #checkpoint_pytorch = '/notebooks/vinet/vinet_v1_01.pt'
+    checkpoint_pytorch = '/notebooks/vinet/checkpoint_0.pt'
     if os.path.isfile(checkpoint_pytorch):
         checkpoint = torch.load(checkpoint_pytorch,\
                             map_location=lambda storage, loc: storage.cuda(0))
@@ -336,19 +336,20 @@ def test():
     
 
     model = Vinet()
-    #model.load_state_dict(checkpoint) # load checkpoint
+    model.load_state_dict(checkpoint)  
     model.cuda()
     model.eval()
-    mydataset = MyDataset('/notebooks/EuRoC_modify/', 'V2_01_easy')
-    #mydataset = MyDataset('/notebooks/data/', 'V2_01_easy')
+    #mydataset = MyDataset('/notebooks/EuRoC_modify/', 'V2_01_easy')
+    datapath = "V2_01_easy"
+    mydataset = MyDataset('/notebooks/data/', datapath)
     
     
     err = 0
     ans = []
     abs_traj = None
     start = 5
-    #for i in range(len(mydataset)-1):
-    for i in range(start, 100):
+    for i in range(start, len(mydataset)-1):
+    #for i in range(start, 100):
         data, data_imu, target, target2 = mydataset.load_img_bat(i, 1)
         data, data_imu, target, target2 = data.cuda(), data_imu.cuda(), target.cuda(), target2.cuda()
 
@@ -376,6 +377,7 @@ def test():
         abs_traj = Variable(torch.from_numpy(abs_traj).type(torch.FloatTensor).cuda()) 
         
         ans.append(xyzq)
+        print(xyzq)
         print('{}/{}'.format(str(i+1), str(len(mydataset)-1)) )
         
         
@@ -385,7 +387,8 @@ def test():
     x = trajectoryAbs[0].astype(str)
     x = ",".join(x)
     
-    with open('/notebooks/EuRoC_modify/V2_01_easy/vicon0/sampled_relative_ans.csv', 'w+') as f:
+    #with open('/notebooks/EuRoC_modify/V2_01_easy/vicon0/sampled_relative_ans.csv', 'w+') as f:
+    with open('/notebooks/vinet/%s_sampled_relative_ans.csv'%datapath, 'w') as f:
         tmpStr = x
         f.write(tmpStr + '\n')        
         
@@ -397,9 +400,12 @@ def test():
             f.write(tmpStr + '\n')      
     
 def main():
-    #train()
+    train()
           
-    test()
+    #test()
+
+    
+        
 
 if __name__ == '__main__':
     main()
